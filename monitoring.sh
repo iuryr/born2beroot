3c7ad3b51d16f8b21b4e51631b7c2a76fd2e84af
Iury Santos <iuryroberto@gmail.com>
2023-07-17 17:38:40 -0300

    cpu load

--- monitoring.sh
+++ monitoring.sh
@@ -6,14 +6,18 @@ virtual_cpu=`lscpu | grep CPU\(s\): | awk '{print $2}'`
 
 free_ram=`free --mega | awk 'FNR == 2 {print $3}'`
 total_ram=`free --mega | awk 'FNR == 2 {print $2}'`
-pct_ram_usage=`echo "scale=2;$free_ram/$total_ram" | bc`
+pct_ram_usage=`echo "scale=2; $free_ram / $total_ram" | bc`
 
 used_disk_MB=`df /dev/sdb --block-size=1MB --output=used | awk 'FNR==2 {print $1}'`
 total_disk_GB=`df /dev/sdb --block-size=1GB --output=size | awk 'FNR==2 {print $1}'`
 used_disk_pct=`df /dev/sdb --output=pcent | awk 'FNR==2 {print $1}'`
+
+cpu_idle_pct=`top -bn 1 | grep %Cpu | tail -1 | awk '{print $8}'`
+cpu_usage_pct=`echo "scale=1; 100 - $cpu_idle_pct" | bc`
 
 printf "#Architecture: $architecture\n"
 printf "#CPU Physical: $phys_cpu\n"
 printf "#vCPU: $virtual_cpu\n"
 printf "#Memory Usage: %d/%dMB (%.2f%%)\n" $free_ram $total_ram $pct_ram_usage
 printf "#Disk Usage: %d/%dGb (%s)\n" $used_disk_MB $total_disk_GB $used_disk_pct
+printf "#CPU load: %.1f%%\n" $cpu_usage_pct
