7e48a43679516421609ca31e9bb908f091c4f169
Iury Santos <iuryroberto@gmail.com>
2023-07-17 16:20:18 -0300

    disk usage

--- monitoring.sh
+++ monitoring.sh
@@ -8,7 +8,12 @@ free_ram=`free --mega | awk 'FNR == 2 {print $3}'`
 total_ram=`free --mega | awk 'FNR == 2 {print $2}'`
 pct_ram_usage=`echo "scale=2;$free_ram/$total_ram" | bc`
 
+used_disk_MB=`df /dev/sdb --block-size=1MB --output=used | awk 'FNR==2 {print $1}'`
+total_disk_GB=`df /dev/sdb --block-size=1GB --output=size | awk 'FNR==2 {print $1}'`
+used_disk_pct=`df /dev/sdb --output=pcent | awk 'FNR==2 {print $1}'`
+
 printf "#Architecture: $architecture\n"
 printf "#CPU Physical: $phys_cpu\n"
 printf "#vCPU: $virtual_cpu\n"
-printf "#Memory Usage: %d/%dMB (%.2f%%)" $free_ram $total_ram $pct_ram_usage
+printf "#Memory Usage: %d/%dMB (%.2f%%)\n" $free_ram $total_ram $pct_ram_usage
+printf "#Disk Usage: %d/%dGb (%s)\n" $used_disk_MB $total_disk_GB $used_disk_pct
